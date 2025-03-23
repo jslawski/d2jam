74cbc84d5e1c22a56b0e427f1051583d45340383
Jared Slawski <jaredslawski@gmail.com>
2025-03-22 21:25:22 -0600

    New default for global variables, update controls label

--- Assets/_Scripts/GlobalVariables.cs
+++ Assets/_Scripts/GlobalVariables.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class GlobalVariables : MonoBehaviour
 {
-    public static float MAGNETIC_FORCE = 1.0f;
-    public static float RAYCAST_DISTANCE = 2.0f;
+    public static float MAGNETIC_FORCE = 5f;
+    public static float RAYCAST_DISTANCE = 1.0f;
     public static float ENGINE_FORCE = 5.0f;
-    public static float MAX_VELOCITY = 5.0f;
+    public static float MAX_VELOCITY = 7f;
 }
